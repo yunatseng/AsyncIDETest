1accf2dd00a98404e9a67980f3ba8d5135e6016a
effytseng <yy0124125@gmail.com>
2019-03-28 17:24:26 +0800

    ✨：import VueSocketIO、SocketIO

--- codepan/src/index.js
+++ codepan/src/index.js
@@ -7,13 +7,24 @@ import router from '@/router'
 import store from '@/store'
 
 import VueSocketIO from 'vue-socket.io'
-import socketio from 'socket.io-client'
+import SocketIO from 'socket.io-client';
 
 Vue.config.productionTip = false
 
 Vue.use(Tippy, {
   position: 'bottom'
 })
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('localhost:3001'), //options object is Optional
+  vuex: {
+    store,
+    actionPrefix: "SOCKET_",
+    mutationPrefix: "SOCKET_"
+  }
+})
+);
 
 new Vue({
   el: '#app',
